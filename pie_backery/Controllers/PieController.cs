9c844806a8132f6e9954e6b86c7a509a087cc57f
Harsh Jain <31067957+hjain0726@users.noreply.github.com>
2020-02-10 17:06:22 +0530

    initial commit

---
+++ pie_backery/Controllers/PieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using pie_backery.Models;
+using pie_backery.ViewModels;
+
+namespace pie_backery.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {   
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return View(piesListViewModel);
+        }
+    }
+}
